23bdf708ffd6a951a227df102f761df6a8bd7729
TiagoPaulin <thiago.vmp15@gmail.com>
2024-08-01 11:10:18 -0300

    Moving pieces

--- src/main/java/org/example/UI.java
+++ src/main/java/org/example/UI.java
@@ -1,7 +1,11 @@
 package org.example;
 
 import org.example.chess.ChessPiece;
+import org.example.chess.ChessPosition;
 import org.example.chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -39,6 +43,26 @@ public class UI {
             }
         }
         System.out.print(" ");
+    }
+
+    public static ChessPosition readChessPosition(Scanner sc) {
+
+       try {
+
+           String s = sc.nextLine();
+
+           char column = s.charAt(0);
+           int row = Integer.parseInt(s.substring(1));
+
+           return  new ChessPosition(column, row);
+
+       } catch (RuntimeException e) {
+
+           throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
+
+       }
+
+
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
--- src/main/java/org/example/boardgame/Board.java
+++ src/main/java/org/example/boardgame/Board.java
@@ -57,6 +57,28 @@ public class Board {
 
     }
 
+    public Piece removePiece(Position position) {
+
+        if (!positionExists(position.getRow(), position.getColumn())) {
+
+            throw new BoardException("Position not on the board");
+
+        }
+
+        if(piece(position) == null) {
+
+            return null;
+
+        }
+
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+
+        return aux;
+
+    }
+
     public boolean positionExists(Position position) {
 
         return positionExists(position.getRow(), position.getColumn());
